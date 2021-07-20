e35ea220ea97c87029696c01b7641da1541315d4
p0ssuidao <22448987+P0ssuidao@users.noreply.github.com>
2021-07-20 00:26:58 -0300

    Fix files

--- DigitalOcean/variable.tf
+++ DigitalOcean/variable.tf
@@ -7,12 +7,7 @@ variable "key_name" {
   type = string
   default = "wirekey"
 }
-<<<<<<< HEAD
-variable "do_token" {}
-=======
 
 variable "do_token" {
   type = string
 }
->>>>>>> d74b97747d6a5d97b86583f72d64b1c08652a318
-
